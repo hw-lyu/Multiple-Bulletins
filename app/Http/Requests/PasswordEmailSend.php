5cfa2a6ddd638be66b3854ccedbb1ed915cd412e
yuhyewon <lyuhye03@gmail.com>
2023-05-21 23:08:08 +0900

    refactor : 패스워드 이메일 보낼 때 - 밸리데이션 유호성 검사 속성값 및 룰 추가

--- app/Http/Requests/PasswordEmailSend.php
+++ app/Http/Requests/PasswordEmailSend.php
@@ -8,10 +8,32 @@ class PasswordEmailSend extends FormRequest
 {
   /**
    * Http/Controllers/PasswordController.php | emailSend() rules
-   * @return string[]
+   * @return array
    */
   public function rules(): array
   {
     return ['email' => 'required|email'];
+  }
+
+  /**
+   * 유효성 감사 속성 값
+   * @return array
+   */
+  public function attributes() : array
+  {
+    return [
+      "email" => "게시판 이메일 주소"
+    ];
+  }
+
+  /**
+   * 유효성검사 메시지
+   * @return array
+   */
+  public function messages(): array
+  {
+    return [
+      'email' => ':attribute룰 다시 확인해주세요.'
+    ];
   }
 }
