d2e6f3846c0389ab402f3c3d6256b59248c709c4
yuhyewon <lyuhye03@gmail.com>
2023-05-25 22:23:49 +0900

    Comment : 모델 Board 주석 추가

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -7,13 +7,52 @@ use App\Traits\SerializeDate;
 
 class Board extends Model
 {
-  use SerializeDate;
+    /**
+     * SerializeDate
+     */
+    use SerializeDate;
 
-  protected $table = 'board_basic';
-  protected $primaryKey = 'idx';
-  
-  protected $fillable = ['user_email', 'board_cate', 'board_title', 'views', 'view_like', 'board_content', 'photo_state', 'all_comment'];
+    /**
+     * 모델과 연결된 테이블 정의
+     *
+     * @var string
+     */
+    protected $table = "board_basic";
 
-  const CREATED_AT = 'view_created_at';
-  const UPDATED_AT = 'view_updated_at';
+    /**
+     * 모델의 기본 키 정의
+     *
+     * @var string
+     */
+    protected $primaryKey = "idx";
+
+    /**
+     * 대량 할당(Mass Assignment)할 모델의 속성을 정의
+     *
+     * @var []
+     */
+    protected $fillable = [
+        "user_email",
+        "board_cate",
+        "board_title",
+        "views",
+        "view_like",
+        "board_content",
+        "photo_state",
+        "all_comment",
+    ];
+
+    /**
+     * 작성 시간 열의 이름 정의
+     *
+     * @var string
+     */
+    const CREATED_AT = "view_created_at";
+
+    /**
+     * 업데이트 시간 열의 이름 정의
+     *
+     * @var string
+     */
+    const UPDATED_AT = "view_updated_at";
 }
